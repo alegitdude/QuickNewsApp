501358c8b01e2d45ab457c6059dec4b7c5aeb3c9
Rick Hughey <hughey.thomas@gmail.com>
2024-01-23 18:50:57 -0600

    "publish

--- Persistence/obj/Debug/net7.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net7.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6376bd9a01786234e0b591de532a378b157ec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0402872c4aad45963df1f91cef67fbe0befd840a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
