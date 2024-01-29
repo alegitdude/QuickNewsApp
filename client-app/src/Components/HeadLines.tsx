190258e34accc9e78dd304f1bcbbe19e9a60c778
Rick Hughey <hughey.thomas@gmail.com>
2024-01-29 11:55:18 -0600

    "load more button"

--- client-app/src/Components/HeadLines.tsx
+++ client-app/src/Components/HeadLines.tsx
@@ -8,31 +8,50 @@ import { NewsState } from "../models/newsState";
 
 import SingleArticle from "./SingleArticle";
 import { ToastContainer, Zoom } from "react-toastify";
+import {
+  BusinessIcon,
+  EntertainmentIcon,
+  GeneralIcon,
+  PoliticsIcon,
+  SportsIcon,
+  TechIcon,
+} from "../assets/Index";
 
 const HeadLines = () => {
   const [fetchedNews, setFetchedNews] = useState<Article[]>();
+  const [imageNumber, setImageNumber] = useState<number>(60);
+  const [iconVariable, setIconVariable] = useState<JSX.Element>(
+    <GeneralIcon />
+  );
   const theUrl = useLocation().pathname;
   const news: NewsState = useSelector((store: RootState) => store.news);
 
   useEffect(() => {
+    setImageNumber(60);
     switch (theUrl.substring(1, theUrl.length)) {
       case "general":
         setFetchedNews(news.general);
+        setIconVariable(<GeneralIcon />);
         break;
       case "politics":
         setFetchedNews(news.politics);
+        setIconVariable(<PoliticsIcon />);
         break;
       case "business":
         setFetchedNews(news.business);
+        setIconVariable(<BusinessIcon />);
         break;
       case "tech":
         setFetchedNews(news.tech);
+        setIconVariable(<TechIcon />);
         break;
       case "sports":
         setFetchedNews(news.sports);
+        setIconVariable(<SportsIcon />);
         break;
       case "entertainment":
         setFetchedNews(news.entertainment);
+        setIconVariable(<EntertainmentIcon />);
         break;
     }
   }, [theUrl, news]);
@@ -43,8 +62,9 @@ const HeadLines = () => {
     return (
       <div className="flex flex-col items-center m-4">
         <ToastContainer transition={Zoom} limit={2} />
-        <div>
-          <h1 className="mb-4 text-3xl">Breaking</h1>
+        <div className="flex">
+          <h1 className="mb-4 text-3xl">Breaking - </h1>
+          <div className="ml-2 text-4xl"> {iconVariable}</div>
         </div>
         <div className="relative grid gap-0 mb-4 sm:gap-4 grid-col-1 sm:grid-cols-2 md:grid-cols-3 w-7/8 h">
           {slicedNews?.map((oneArticle) => {
@@ -55,7 +75,7 @@ const HeadLines = () => {
         </div>
 
         <div className="flex flex-col gap-2 md:grid md:grid-cols-2 md:gap-4 ">
-          {fetchedNews.slice(10).map((article) => {
+          {fetchedNews.slice(10, imageNumber).map((article) => {
             return (
               <div className="h-fit" key={article.uuid}>
                 <SingleArticle article={article} />
@@ -63,6 +83,20 @@ const HeadLines = () => {
             );
           })}
         </div>
+        <button
+          className={`flex-none transition-all rounded-md bg-primary my-10 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent ${
+            imageNumber == fetchedNews.length ? "hidden" : ""
+          }`}
+          onClick={() => {
+            if (fetchedNews.length - 10 > imageNumber + 50) {
+              setImageNumber((imageNumber) => imageNumber + 50);
+            } else {
+              setImageNumber(() => fetchedNews.length);
+            }
+          }}
+        >
+          Load more
+        </button>
       </div>
     );
   }
--- client-app/src/assets/Index.ts
+++ client-app/src/assets/Index.ts
@@ -1,2 +1,8 @@
 export { default as IconNewsPaper } from "./IconNewsPaper";
 export { default as IconHamburgerMenu } from "./IconHamburgerMenu";
+export { default as BusinessIcon } from "./BusinessIcon";
+export { default as GeneralIcon } from "./GeneralIcon";
+export { default as PoliticsIcon } from "./PoliticsIcon";
+export { default as TechIcon } from "./TechIcon";
+export { default as SportsIcon } from "./SportsIcon";
+export { default as EntertainmentIcon } from "./EntertainmentIcon";
